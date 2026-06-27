8707d3a0b39b11477f0a9a082b162c6f0515ebc9
casiano-reyes <torrid.nights@gmail.com>
2026-06-27 13:02:25 -0500

    Debug: log available env vars on startup

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
   console.error('FATAL: JWT_SECRET no está definido en las variables de entorno');
+  console.error('Variables disponibles:', Object.keys(process.env).join(', '));
   process.exit(1);
 }
 
